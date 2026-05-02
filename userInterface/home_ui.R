27c1589879657162ec5a290bb54d898ace4d8427
Samuel Bharti <sbharti@uab.edu>
2026-05-01 23:32:13 -0500

    Improve home page template

--- userInterface/home_ui.R
+++ userInterface/home_ui.R
@@ -1,9 +1,20 @@
 home_page <- fluidPage(
-  # Page title
-  titlePanel("Home Page"),
+  titlePanel("Home"),
   hr(),
   fluidRow(
-    column(6, h2("Column size 6")),
-    column(6, h2("Column size 6"))
+    column(
+      width = 8,
+      p("Start here: customize this page with your app introduction."),
+      example_counter_ui("home_counter")
+    ),
+    column(
+      width = 4,
+      tags$h4("Quick Notes"),
+      tags$ul(
+        tags$li("Place shared controls in modules/"),
+        tags$li("Keep page layouts in userInterface/"),
+        tags$li("Keep heavy compute out of render blocks")
+      )
+    )
   )
 )
